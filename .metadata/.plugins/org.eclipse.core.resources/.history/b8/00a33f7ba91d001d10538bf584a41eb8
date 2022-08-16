7cc5cd29d40069be0db4f37d13d813492a65efe3
BenBrassard <bj.brassard@gmail.com>
2022-08-16 16:22:54 -0500

    Added annotations to restaurantModel

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b8/00a33f7ba91d001d10538bf584a41eb8
@@ -0,0 +1,19 @@
+package com.garden.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class restaurantModel {
+	
+	private int id;
+	private String name;
+	private String catergory;
+	private String address;
+	private String contact;
+	
+
+}
--- DBMT_AbundantGardens/com.garden/src/main/java/com/garden/model/restaurantModel.java
+++ DBMT_AbundantGardens/com.garden/src/main/java/com/garden/model/restaurantModel.java
@@ -7,12 +7,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+
+@Entity
+
+@Table(name = "restaurants")
+
 public class restaurantModel {
 	
-	private int id;
-	private String name;
+	@Id
+	@Column(name = "restId")
+	
+	@GeneratedValue(generator = "restaurant_id_seq", strategy = GenerationType.AUTO)
+	@SequenceGenerator(allocationSize = 1, name = "restaurant_id_seq")
+	private int restId;
+	@Column(name = "restName")
+	private String restName;
+	@Column(name = "catergory")
 	private String catergory;
+	@Column(name = "address")
 	private String address;
+	@Column(name = "contact")
 	private String contact;
 	
 
